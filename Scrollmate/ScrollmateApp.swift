f35a12a2ddd774e9a63123679b1fbb25b43e24cc
noyhkos <skhn00j@gmail.com>
2026-02-11 19:58:47 +0900

    1

---
+++ Scrollmate/ScrollmateApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollmateApp.swift
+//  Scrollmate
+//
+//  Created by 김석현 on 2/11/26.
+//
+
+import SwiftUI
+
+@main
+struct ScrollmateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ScrollmateTests/ScrollmateTests.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollmateTests.swift
+//  ScrollmateTests
+//
+//  Created by 김석현 on 2/11/26.
+//
+
+import Testing
+@testable import Scrollmate
+
+struct ScrollmateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
